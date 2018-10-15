bac531be82722dbc7877a8b402ad0f6743a6d9f2
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-10-15 14:22:20 -0400

    implement option in control card

--- src/dpmjet3.0-5F-new.f
+++ src/dpmjet3.0-5F-new.f
@@ -4901,11 +4901,12 @@ C            ENDIF
             LSTART = .FALSE.
          ENDIF
     1    CONTINUE
-         IF (IFMDIST) THEN
-            CALL DT_FFERMI(PABS)
-         ELSE
-            CALL DT_DFERMI(PABS)
-         ENDIF
+         CALL DT_FFERMI(PABS)
+         ! IF (IFMDIST) THEN
+         !    CALL DT_FFERMI(PABS)
+         ! ELSE
+         !    CALL DT_DFERMI(PABS)
+         ! ENDIF
          PABS = PFERM*PABS
 C        IF (PABS.GE.PBIND) THEN
 C           ILOOP = ILOOP+1
