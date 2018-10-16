8b44d472e4e3944e59fbff6c73a14f18e9abc13d
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-10-16 19:05:28 -0400

    test

--- src/dpmjet3.0-5F-new.f
+++ src/dpmjet3.0-5F-new.f
@@ -17343,7 +17343,7 @@ C     SID = SQRT((ONE-COD)*(ONE+COD))
       IF (G(3).LT.G(2)) GOTO 40
       IF (G(3).LT.G(1)) GOTO 30
       GGPART = G(3)
-   20 RETURN GGPART
+   20 RETURN
    30 GGPART = G(1)
       GOTO 20
    40 IF (G(2).LT.G(1)) GOTO 30
