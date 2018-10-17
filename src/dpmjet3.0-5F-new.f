6fabb423c4f9f1aa77f8733c1e8828a53753ec99
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-10-17 17:33:33 -0400

    test

--- src/dpmjet3.0-5F-new.f
+++ src/dpmjet3.0-5F-new.f
@@ -17396,8 +17396,8 @@ C     SID = SQRT((ONE-COD)*(ONE+COD))
         CDF = CDF + (0.001D0/CDFN)*(Z0+Z1+Z2)
         X0 = X0 + 0.001D0
 
-        CDFPLUS = CDF + 0.001D0
-        CDFMINUS = CDF - 0.001D0
+        CDFPLUS = CDF + 0.0015D0
+        CDFMINUS = CDF - 0.0015D0
 
         IF( (C .GE. CDFMINUS) .AND. (C .LT. CDFPLUS) ) THEN
           GGPART = X0
