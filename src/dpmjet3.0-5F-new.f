4f66aeb530fd02b4920ecb154a5d566ae23fa1c7
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2019-05-02 08:35:02 -0400

    Problem with irej flag

--- src/dpmjet3.0-5F-new.f
+++ src/dpmjet3.0-5F-new.f
@@ -5016,7 +5016,7 @@ C            ENDIF
           WRITE(*,*) 'mass: ', PHKK(5,K2)
 
           CALL DT_KFERMI(P00,IFMDIST) !re-sample momentum using deuteron high momentum tail
-          P00=P00*FERMOD
+          P00=0.1D0
           WRITE(*,*) 'Fermi momentum P00 ', P00
           WRITE(*,*) 'Distance (fm) scale ~ ', 0.197D0/P00
           CALL DT_DPOLI(POLC,POLS)
